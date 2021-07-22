93b8c590c6ccfa96c4f66bd4fdc027ef33ed01f4
sakshi <skhanduri9700@gmail.com>
2021-07-22 14:50:34 +0530

    changed color and label text

--- second_window.py
+++ second_window.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from Add_Item import AddItems
 
+
 #create a class of Display Category
 class DisplayCategory(Toplevel):
     def __init__(self):
@@ -12,23 +13,20 @@ class DisplayCategory(Toplevel):
 
 
         #Top Frame
-        self.top = Frame(self, width=1199, height=180, bg="red")
+        self.top = Frame(self, width=1199, height=180, bg="#d9c8e3")
         self.top.pack(fill=X)
 
         #Top Frame Design
-        Label(self.top, text="Display Items", font="times 28 bold", width=18, height=2, 
-        fg="white", bg="red", bd=2).place(x=415, y=1)
+        Label(self.top, text="Welcome to Apna Mart !!", font="times 32 bold", width=18, height=2, 
+        fg="#5d0f8a", bg="#d9c8e3", bd=2).place(x=415, y=1)
         
-        #button 
+        #  Add Button 
         self.add_items = Button(self.top, text="Add Items", font="times 14 bold", 
-        width="15",height="2",bg="deep sky blue",fg="white",bd=0, command=self.Additems)
+        width="15",height="2",bg="#594e78", fg="white",bd=0, command=self.Additems)
         self.add_items.place(x=1000, y=85)
 
-
-
-
-        #Bottom Frame
-        self.bottom = Frame(self, width=1199, height=370, bg="blue")
+        # Bottom Frame
+        self.bottom = Frame(self, width=1199, height=370, bg="#8343ab")
         self.bottom.pack(fill=X)
 
 
